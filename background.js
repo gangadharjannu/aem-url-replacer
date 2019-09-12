ab6dfc1e6ed0c28061830b22135b91ea6248bcf8
Gangadhara Jannu <gangadhara.jannu@EUIL-MAC021.local>
2019-09-12 16:05:38 +0200

    Updated background.js filtering logic

--- background.js
+++ background.js
@@ -1,14 +1,10 @@
-chrome.webNavigation.onBeforeNavigate.addListener(function (details) {
-    var AEM_URL = 'http://localhost:4502/content/';
+chrome.webNavigation.onBeforeNavigate.addListener((details) => {
+    const AEM_URL = 'http://localhost:4502/content/';
+    const requestURL = details.url;
 
-    function updatedURL(url) {
-        // return url.slice(-1) === '/' ? url.slice(0, -1) + '.html' : url + '.html';
-        return (url + '.html').replace(/\/\./, '.');
-    }
-
-    if (details.url.indexOf(AEM_URL) !== -1 && details.url.indexOf('.html') === -1) {
+    if (requestURL.indexOf(AEM_URL) !== -1 && requestURL.indexOf('.html') === -1) {
         chrome.tabs.update(details.tabId, {
-            url: updatedURL(details.url)
+            url: (requestURL + '.html').replace(/\/\./, '.')
         });
     }
 });
